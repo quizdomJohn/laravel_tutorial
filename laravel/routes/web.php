b7ecc420d3cda5b266090b82c56759e2038522fc
John Tsatsanis <john.tsatsanis@quizdom.gr>
2024-11-21 10:57:18 +0200

    make 'create post' button to work

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;  //import it
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ExampleController;
+use App\Http\Controllers\UserController;  //import it
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +16,12 @@ use App\Http\Controllers\ExampleController;
 |
 */
 
+// User related routes
 Route::get('/',[UserController::class,"showCorrectHomepage"]); //modified so it will show a template depending if the user is logged in
 Route::get('/about',[ExampleController::class,"aboutpage"]);
 Route::post('/register',[UserController::class,'register']);
 Route::post('/login',[UserController::class,'login']);
 Route::post('/logout',[UserController::class,'logout']);
+
+// Posts related routes
+Route::get('/create-post',[PostController::class,'showCreateForm']);
