0ec02f676f256e2b15d35806636e4bd16a64e887
John Tsatsanis <john.tsatsanis@quizdom.gr>
2024-11-22 16:33:44 +0200

    make the edit button to work

--- laravel/resources/views/single-post.blade.php
+++ laravel/resources/views/single-post.blade.php
@@ -6,7 +6,7 @@
 
         @can('update',$post) {{-- can the update be used for this post??? --}}
         <span class="pt-2">
-          <a href="#" class="text-primary mr-2" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fas fa-edit"></i></a>
+          <a href="/post/{{$post->id}}/edit" class="text-primary mr-2" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fas fa-edit"></i></a>
           <form class="delete-post-form d-inline" action="/post/{{$post->id}}" method="POST">
             @csrf
             @method('DELETE') {{-- for bypassing the 'POST method --}}
